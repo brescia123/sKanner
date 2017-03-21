a2e5af0f42c6502a3ddb49e9b73563f0836af1e0
Giacomo Bresciani <brescia123@gmail.com>
2017-03-21 18:02:35 +0100

    Add TravisCI support

--- src/main/java/it/facile/skanner/main/Skanner.kt
+++ src/main/java/it/facile/skanner/main/Skanner.kt
@@ -1,3 +1,5 @@
+@file:JvmName("sKanner")
+
 package it.facile.skanner.main
 
 import android.graphics.Bitmap
@@ -6,17 +8,15 @@ import org.opencv.core.CvType
 import org.opencv.core.Mat
 import org.opencv.imgproc.Imgproc
 
-object Skanner {
-    fun detectEdges(bitmap: Bitmap): Bitmap {
-        val colorImage = Mat()
-        Utils.bitmapToMat(bitmap, colorImage)
+fun detectEdges(bitmap: Bitmap): Bitmap {
+    val colorImage = Mat()
+    Utils.bitmapToMat(bitmap, colorImage)
 
-        val edges = Mat(colorImage.size(), CvType.CV_8UC1)
-        Imgproc.cvtColor(colorImage, edges, Imgproc.COLOR_RGB2GRAY, 4)
-        Imgproc.Canny(edges, edges, 80.0, 100.0)
+    val edges = Mat(colorImage.size(), CvType.CV_8UC1)
+    Imgproc.cvtColor(colorImage, edges, Imgproc.COLOR_RGB2GRAY, 4)
+    Imgproc.Canny(edges, edges, 80.0, 100.0)
 
-        val resultBitmap = Bitmap.createBitmap(edges.cols(), edges.rows(), Bitmap.Config.ARGB_8888)
-        Utils.matToBitmap(edges, resultBitmap)
-        return resultBitmap
-    }
+    val resultBitmap = Bitmap.createBitmap(edges.cols(), edges.rows(), Bitmap.Config.ARGB_8888)
+    Utils.matToBitmap(edges, resultBitmap)
+    return resultBitmap
 }
